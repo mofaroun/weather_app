18079d28be930fc162c7c1863323e5bc19666756
Mohammed Faroun <farounmohammed@gmail.com>
2024-08-31 23:45:27 -0500

    weather app

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,84 @@
+import "package:flutter/material.dart";
+import "package:lottie/lottie.dart";
+import "package:weather_app/models/weather_model.dart";
+import "package:weather_app/services/weather_service.dart";
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('bbf6590c6a8dd1f363037fa49b1ad952');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/sunnyrainy.json';
+      case 'thunderstorm':
+        return 'assets/thunderstorm.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // City
+            Text(
+              _weather?.cityName ?? "loading city",
+            ),
+
+            // Animation
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+            // Temperature
+            Text('${_weather?.temperature.round()}°C'),
+
+            // Condition
+            Text(_weather?.mainCondition ?? "")
+          ],
+        ),
+      ),
+    );
+  }
+}
